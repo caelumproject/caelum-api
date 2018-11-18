821345f549a7f35f145f73b1b91b0b9a3d4a1351
caelumdev <walschot.vanstalle@gmail.com>
2018-11-18 11:07:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(6000, function () {
+var server = app.listen(6050, function () {
     console.log("app running on port.", server.address().port);
 });
